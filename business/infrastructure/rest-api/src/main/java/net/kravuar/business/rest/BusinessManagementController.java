eb9f4cce90618b1f0b49cb617dd255621b6fd751
kravuar <steam_webmoney@mail.ru>
2024-02-09 15:25:34 +0300

    fix containerization, basic rest infrastructure module for business

---
+++ business/infrastructure/rest-api/src/main/java/net/kravuar/business/rest/BusinessManagementController.java
@@ -0,0 +1,43 @@
+package net.kravuar.business.rest;
+
+import lombok.RequiredArgsConstructor;
+import net.kravuar.business.domain.Business;
+import net.kravuar.business.domain.commands.BusinessChangeEmailCommand;
+import net.kravuar.business.domain.commands.BusinessChangeNameCommand;
+import net.kravuar.business.domain.commands.BusinessCreationCommand;
+import net.kravuar.business.domain.commands.BusinessEmailVerificationCommand;
+import net.kravuar.business.domain.exceptions.MessageSendingException;
+import net.kravuar.business.ports.in.BusinessManagementUseCase;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/management")
+@RequiredArgsConstructor
+public class BusinessManagementController {
+    // TODO: annoying CouldNotAutowire, but its not an app
+    private final BusinessManagementUseCase businessManagement;
+
+    // TODO: do i need dto's?
+
+    @PostMapping("/register")
+    public Business register(BusinessCreationCommand command) throws MessageSendingException {
+        return businessManagement.create(command);
+    }
+
+    @PostMapping("/change-name")
+    public void changeName(BusinessChangeNameCommand command) {
+        businessManagement.changeName(command);
+    }
+
+    @PostMapping("/change-email")
+    public void changeEmail(BusinessChangeEmailCommand command) throws MessageSendingException {
+        businessManagement.changeEmail(command);
+    }
+
+    @PostMapping("/verify-email")
+    public boolean verifyEmail(BusinessEmailVerificationCommand command) {
+        return businessManagement.verifyEmail(command);
+    }
+}
