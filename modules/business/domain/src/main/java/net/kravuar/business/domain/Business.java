90c33f2c673b316c6789f5babefa6931390b93e0
kravuar <steam_webmoney@mail.ru>
2024-03-05 22:03:30 +0300

    forgor

--- modules/business/domain/src/main/java/net/kravuar/business/domain/Business.java
+++ modules/business/domain/src/main/java/net/kravuar/business/domain/Business.java
@@ -2,14 +2,16 @@ package net.kravuar.business.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Business {
     private Long id;
-    private final String ownerSub;
+    private String ownerSub;
     private String name;
     private boolean active;
 
